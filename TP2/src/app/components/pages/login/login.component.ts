9e405c5763db1ff466e81a0485c515c363376aad
Manuela Fernandez <manu.fer.0396@gmail.com>
2022-05-24 06:23:58 -0300

    Improve login page

--- TP2/src/app/components/pages/login/login.component.ts
+++ TP2/src/app/components/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { UserLogin } from "src/app/interfaces/user-login";
+import { SessionService } from "src/app/services/session.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+	user: UserLogin;
+	password: string = "";
+	spinnerLoading: boolean = false;
+  successLoginOverlayVisible: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router,
+		private sessionService: SessionService) {
+      this.user = {
+        email: "",
+        password: "",
+      };
+   }
 
   ngOnInit(): void {
   }
 
+  login(e: any) {
+		e.preventDefault();
+		//TODO: validate inputs (before login and redirect to home)
+		if (
+			// this.user.password.length > 0 &&
+			this.user.password == this.password
+		) {
+			this.spinnerLoading = true;
+			setTimeout(() => {
+				this.sessionService.sigIn(this.user);
+				this.router.navigate(["/"]);
+			}, 2000);
+		} else console.log("TODO: inform errors");
+	}
 }
