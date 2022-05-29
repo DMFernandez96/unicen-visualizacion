8fcf200bd8172b89067da5b41f12c46871a4369f
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-05-28 22:55:07 -0300

    Added favorites service and functionality to for-playing

--- TP2/src/app/components/atoms/card-favorites/card-favorites.component.ts
+++ TP2/src/app/components/atoms/card-favorites/card-favorites.component.ts
@@ -1,16 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { FavoritesService } from "src/app/services/favorites.service";
 
 @Component({
-  selector: 'app-card-favorites',
-  templateUrl: './card-favorites.component.html',
-  styleUrls: ['./card-favorites.component.css']
+	selector: "app-card-favorites",
+	templateUrl: "./card-favorites.component.html",
+	styleUrls: ["./card-favorites.component.css"],
 })
 export class CardFavoritesComponent implements OnInit {
 	@Input() src!: string;
 	@Input() name!: string;
-  constructor() { }
+	@Input() id!: number;
 
-  ngOnInit(): void {
-  }
+	constructor(private favoritesService: FavoritesService) {}
 
+	ngOnInit(): void {}
+
+	removeFavorite(): void {
+		this.favoritesService.removeFavorite(this.id);
+	}
 }
