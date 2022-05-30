8fba4d25ad2a797546390757d8908122bc68a89f
DMFernandez96 <manu.fer.0396@gmail.com>
2022-05-30 00:22:18 +0000

    Deploying to gh-pages from  @ e57b13d4b895b83163b09f2d89aac600a4f276e0 🚀

--- TP2/src/app/components/pages/register/register.component.ts
+++ TP2/src/app/components/pages/register/register.component.ts
@@ -2,40 +2,41 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { UserRegister } from "src/app/interfaces/user-register";
 import { SessionService } from "src/app/services/session.service";
-
 @Component({
-	selector: "app-register",
-	templateUrl: "./register.component.html",
-	styleUrls: ["./register.component.css"],
+ selector: "app-register",
+ templateUrl: "./register.component.html",
+ styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent implements OnInit {
-	user: UserRegister;
-	confirmPassword: string = "";
-
-	constructor(
-		private router: Router,
-		private sessionService: SessionService
-	) {
-		this.user = {
-			username: "",
-			email: "",
-			password: "",
-		};
-	}
-
-	ngOnInit(): void {}
-
-	register(e: any) {
-		e.preventDefault();
-		//TODO: validate inputs (before register and redirect to home)
-		if (
-			// this.user.password.length > 0 &&
-			this.user.password == this.confirmPassword
-		) {
-			setTimeout(() => {
-				this.sessionService.register(this.user);
-			}, 500);
-			this.router.navigate(["/"]);
-		} else console.log("TODO: inform errors");
-	}
+ user: UserRegister;
+ confirmPassword: string = "";
+ isShown: boolean = false; // hidden by default
+ constructor(
+  private router: Router,
+  private sessionService: SessionService
+ ) {
+  this.user = {
+   username: "",
+   email: "",
+   password: "",
+  };
+ }
+ ngOnInit(): void {}
+ register(e: any) {
+  e.preventDefault();
+  //TODO: validate inputs (before register and redirect to home)
+  if (
+   // this.user.password.length > 0 &&
+   this.user.password == this.confirmPassword
+  ) {
+   setTimeout(() => {
+    this.sessionService.register(this.user);
+    this.router.navigate(["/"]);
+   }, 2000);
+  
+  } else console.log("TODO: inform errors");
+ }
+ toggleShow(): void {
+   this.isShown = !this.isShown;
+ }
 }
