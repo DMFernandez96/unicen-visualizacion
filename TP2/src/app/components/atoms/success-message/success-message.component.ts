4f5e2c96d9299b9cf9f00ec4966430fe798369ce
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-05-30 12:52:43 -0300

    Added validations to register

--- TP2/src/app/components/atoms/success-message/success-message.component.ts
+++ TP2/src/app/components/atoms/success-message/success-message.component.ts
@@ -1,21 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 @Component({
-  selector: 'app-success-message',
-  templateUrl: './success-message.component.html',
-  styleUrls: ['./success-message.component.css']
+	selector: "app-success-message",
+	templateUrl: "./success-message.component.html",
+	styleUrls: ["./success-message.component.css"],
 })
 export class SuccessMessageComponent implements OnInit {
-  show: boolean = false;
-  @Output() messageShow = new EventEmitter<boolean>();
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  toggleShow(): void {
-    this.show = true;
-      this.messageShow.emit(this.show);
-  }
-
+	ngOnInit(): void {}
 }
