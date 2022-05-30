e57b13d4b895b83163b09f2d89aac600a4f276e0
Manuela Fernandez <manu.fer.0396@gmail.com>
2022-05-29 21:20:22 -0300

    Added success message on forms

--- TP2/src/app/components/pages/profile/profile.component.ts
+++ TP2/src/app/components/pages/profile/profile.component.ts
@@ -2,40 +2,35 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { UserRegister } from "src/app/interfaces/user-register";
 import { NgZone } from "@angular/core";
-
 @Component({
-	selector: "app-profile",
-	templateUrl: "./profile.component.html",
-	styleUrls: ["./profile.component.css"],
+ selector: "app-profile",
+ templateUrl: "./profile.component.html",
+ styleUrls: ["./profile.component.css"],
 })
 export class ProfileComponent implements OnInit {
-	user: UserRegister;
-	confirmPassword: string = "";
-	isShown: boolean = false; // hidden by default
-
-	constructor(private router: Router) {
-		this.user = {
-			username: "",
-			email: "",
-			password: "",
-		};
-	}
-
-	ngOnInit(): void {}
-
-	editProfile(e: any) {
-		e.preventDefault();
-		if (
-			// this.user.password.length > 0 &&
-			this.user.password == this.confirmPassword
-		) {
-			this.router.navigate(["/"]);
-		} else console.log("TODO: inform errors");
-	}
-
-	toggleShow(): void {
-		setTimeout(() => {
-			this.isShown = !this.isShown;
-		}, 1000);
-	}
+ user: UserRegister;
+ confirmPassword: string = "";
+ isShown: boolean = false; // hidden by default
+ constructor(private router: Router) {
+  this.user = {
+   username: "",
+   email: "",
+   password: "",
+  };
+ }
+ ngOnInit(): void {}
+ editProfile(e: any) {
+  e.preventDefault();
+  if (
+   // this.user.password.length > 0 &&
+   this.user.password == this.confirmPassword
+  ) {
+   setTimeout(() => {
+   this.router.navigate(["/"]);
+  }, 2000);
+  } else console.log("TODO: inform errors");
+ }
+ toggleShow(): void {
+   this.isShown = !this.isShown;
+ }
 }
