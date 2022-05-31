c67f6f05d6aad4fddfeb141db1763acdf1f7d8df
Manuela Fernandez <manu.fer.0396@gmail.com>
2022-05-30 21:58:00 -0300

    Added info-overlay on for-playing page

--- TP2/src/app/components/pages/for-playing/for-playing.component.ts
+++ TP2/src/app/components/pages/for-playing/for-playing.component.ts
@@ -1,55 +1,57 @@
 import { Component, OnInit } from "@angular/core";
 import { FavoritesService } from "src/app/services/favorites.service";
 import { SessionService } from "src/app/services/session.service";
-
 @Component({
-	selector: "app-for-playing",
-	templateUrl: "./for-playing.component.html",
-	styleUrls: ["./for-playing.component.css"],
+ selector: "app-for-playing",
+ templateUrl: "./for-playing.component.html",
+ styleUrls: ["./for-playing.component.css"],
 })
 export class ForPlayingComponent implements OnInit {
-	tabSelected: number = 1;
-	id: number = 17;
-	isFavorite: boolean = false;
-	like!: boolean | undefined;
+ tabSelected: number = 1;
+ id: number = 17;
+ isFavorite: boolean = false;
+ like!: boolean | undefined;
+ infoOverlayVisible: boolean = false;
+ constructor(
+  private favoritesService: FavoritesService,
+  private sessionService: SessionService
+ ) {}
+ ngOnInit(): void {
+  this.isFavorite = this.updateFavorite();
+  
+ }
+ toggleFavorite() {
+  this.favoritesService.toggleFavorite(this.id);
+  this.isFavorite = this.updateFavorite();
+ }
+ updateFavorite(): boolean {
+  if (
+   this.sessionService.isLoggedIn() &&
+   this.favoritesService.isFavorite(this.id)
+  ) {
+   return true;
+  } else return false;
+ }
+ isLoggedIn(): boolean {
+  return this.sessionService.isLoggedIn();
+ }
+ toggleLike(state: string) {
+  if (this.like == undefined) {
+   this.like = state == "like";
+  } else {
+   if (
+    (this.like && state == "like") ||
+    (!this.like && state == "dislike")
+   )
+    this.like = undefined;
+   else this.like = !this.like;
+  }
+ }
 
-	constructor(
-		private favoritesService: FavoritesService,
-		private sessionService: SessionService
-	) {}
-
-	ngOnInit(): void {
-		this.isFavorite = this.updateFavorite();
-	}
-
-	toggleFavorite() {
-		this.favoritesService.toggleFavorite(this.id);
-		this.isFavorite = this.updateFavorite();
-	}
-
-	updateFavorite(): boolean {
-		if (
-			this.sessionService.isLoggedIn() &&
-			this.favoritesService.isFavorite(this.id)
-		) {
-			return true;
-		} else return false;
-	}
-
-	isLoggedIn(): boolean {
-		return this.sessionService.isLoggedIn();
-	}
-
-	toggleLike(state: string) {
-		if (this.like == undefined) {
-			this.like = state == "like";
-		} else {
-			if (
-				(this.like && state == "like") ||
-				(!this.like && state == "dislike")
-			)
-				this.like = undefined;
-			else this.like = !this.like;
-		}
-	}
+ toggleInfoOverlay(): void {
+  this.infoOverlayVisible = !this.infoOverlayVisible;
+ }
+ closeInfoOverlay(): void {
+  this.infoOverlayVisible = false;
+ }
 }
