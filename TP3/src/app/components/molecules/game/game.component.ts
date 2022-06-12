460525037fd8d48f87c79a54f91f683a5964981a
ezequielcinalli <70036411+ezequielcinalli@users.noreply.github.com>
2022-06-12 14:08:43 +0000

    Deploying to gh-pages from  @ f0c87469bbb5fc21bb9f66ec1363c28db6d72c4a 🚀

--- TP3/src/app/components/molecules/game/game.component.ts
+++ TP3/src/app/components/molecules/game/game.component.ts
@@ -20,12 +20,14 @@ export class GameComponent implements AfterViewInit {
   chipsPerPlayer = 3
 
   chips: Array<Array<Chip>>
+  dropsContainers: Path2D[]
   colorPlayer1: string
   colorPlayer2: string
   turnOfPlayer1: boolean
 
   constructor() {
     this.chips = new Array<Array<Chip>>()
+    this.dropsContainers = []
     this.colorPlayer1 = 'red'
     this.colorPlayer2 = 'yellow'
     this.turnOfPlayer1 = true
@@ -34,8 +36,24 @@ export class GameComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     this.context = this.canvas.nativeElement.getContext('2d')!
+    this.initializeDropsContainers()
     this.initializeBoard()
     this.addPlayerChips()
+  }
+
+  initializeDropsContainers(): void {
+    this.context.strokeStyle = '#fff'
+    for (let i = 0; i < this.boardWidth; i++) {
+      const container = new Path2D()
+      container.rect(
+        this.gapBorder + this.radius * 2 * i + this.gap * i,
+        this.gapBorder,
+        this.radius * 2,
+        this.radius
+      )
+      this.context.stroke(container)
+      this.dropsContainers.push(container)
+    }
   }
 
   initializeBoard(): void {
@@ -45,7 +63,12 @@ export class GameComponent implements AfterViewInit {
         const chip: Chip = new Chip(
           this.context,
           2 * this.radius * i + this.radius + this.gapBorder + this.gap * i,
-          2 * this.radius * j + this.radius + this.gapBorder + this.gap * j,
+          2 * this.radius * j +
+            this.radius +
+            this.gapBorder +
+            this.gap * j +
+            this.gap +
+            this.radius,
           this.radius,
           idCounter
         )
@@ -55,31 +78,44 @@ export class GameComponent implements AfterViewInit {
         idCounter++
       }
     }
-    console.log(this.chips)
   }
 
   canvasRepaint(event: MouseEvent): void {
     const canvasX = this.canvas.nativeElement.getBoundingClientRect().x
     const canvasY = this.canvas.nativeElement.getBoundingClientRect().y
-    //on click the arrow(simulate 1 arrow for each column) paint player color on its column
-    if (
-      // this.context.isPointInPath(
-      //   this.arrow,
-      //   event.clientX - canvasX,
-      //   event.clientY - canvasY
-      // )
-      true
-    ) {
-      console.log('arrow from column "x" clicked, TODO: insert in column "x"')
-      if (this.turnOfPlayer1) {
-        this.chips[5][0].setFillStyle(this.colorPlayer1)
-        this.chips[5][0].draw()
-      } else {
-        this.chips[11][0].setFillStyle(this.colorPlayer2)
-        this.chips[11][0].draw()
+    const x = event.clientX - canvasX
+    const y = event.clientY - canvasY
+    let column = -1
+    for (let i = 0; i < this.dropsContainers.length; i++) {
+      if (this.context.isPointInPath(this.dropsContainers[i], x, y)) {
+        column = i
+        break //break the for is the best practice :D ToDo:improve this
       }
-      this.turnOfPlayer1 = !this.turnOfPlayer1
     }
+    if (column > -1) this.insertInColumn(column)
+  }
+
+  insertInColumn(column: number): boolean {
+    let position = 0
+    let positionEmpty = false
+
+    while (!positionEmpty && position < this.boardHeigth) {
+      console.log(this.chips[column][position].state)
+      if (this.chips[column][position].state != 0) position++
+      else positionEmpty = true
+    }
+    if (position + 1 > this.boardHeigth) return false
+
+    if (this.turnOfPlayer1) {
+      this.chips[column][position].setFillStyle(this.colorPlayer1)
+      this.chips[column][position].state = 1
+    } else {
+      this.chips[column][position].setFillStyle(this.colorPlayer2)
+      this.chips[column][position].state = 2
+    }
+    this.chips[column][position].draw()
+    this.turnOfPlayer1 = !this.turnOfPlayer1
+    return true
   }
 
   addPlayerChips() {
