356764f299ba79f5410749b522d346f243ccd520
ezequielcinalli <70036411+ezequielcinalli@users.noreply.github.com>
2022-06-12 00:29:23 +0000

    Deploying to gh-pages from  @ 7de823d3f146fa191f00fbd00896867c054b9e5c 🚀

--- TP3/src/app/components/molecules/game/Chip.ts
+++ TP3/src/app/components/molecules/game/Chip.ts
@@ -3,12 +3,14 @@ export class Chip {
   posX: number
   posY: number
   radius: number
+  id: number
 
   constructor(
     context: CanvasRenderingContext2D,
     posX: number,
     posY: number,
-    radius: number
+    radius: number,
+    id: number
   ) {
     this.context = context
     this.context.strokeStyle = '#fff'
@@ -16,6 +18,7 @@ export class Chip {
     this.posX = posX
     this.posY = posY
     this.radius = radius
+    this.id = id
   }
 
   draw(): void {
@@ -27,5 +30,16 @@ export class Chip {
 
   setFillStyle(style: string): void {
     this.context.fillStyle = style
+  }
+
+  setPosition(x: number, y: number) {
+    this.posX = x
+    this.posY = y
+    this.draw()
+  }
+
+  IsPointInside(x: number, y: number): boolean {
+    //return if x and y are inside the chip (in other words the chip are clicked)
+    return false
   }
 }
