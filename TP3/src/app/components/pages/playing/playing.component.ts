03e6fee5ac03d3473ee0dd09f794381923d0d2db
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-06-12 19:17:47 -0300

    Refactor classes for game and added partial info-panel

--- TP3/src/app/components/pages/playing/playing.component.ts
+++ TP3/src/app/components/pages/playing/playing.component.ts
@@ -1,20 +1,55 @@
-import { Component, OnInit } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core'
+import { Game } from './Game'
 
 @Component({
   selector: 'app-playing',
   templateUrl: './playing.component.html',
   styleUrls: ['./playing.component.css']
 })
-export class PlayingComponent implements OnInit {
+export class PlayingComponent implements AfterViewInit {
+  @ViewChild('canvas')
+  canvas!: ElementRef<HTMLCanvasElement>
+  game!: Game
   helpOverlayVisible: boolean
+
+  chronometer: any
+  seconds = 0
+  minutes = 0
 
   constructor() {
     this.helpOverlayVisible = false
   }
 
-  ngOnInit(): void {}
+  ngAfterViewInit(): void {
+    this.game = new Game(this.canvas.nativeElement.getContext('2d')!)
+  }
 
   toggleHelpOverlay(): void {
     this.helpOverlayVisible = !this.helpOverlayVisible
+  }
+
+  startChronometer(): void {
+    if (this.chronometer != undefined) {
+      clearInterval(this.chronometer)
+      this.minutes = 0
+      this.seconds = 0
+    }
+    this.chronometer = setInterval(() => {
+      this.seconds++
+      if (this.seconds == 60) {
+        this.seconds = 0
+        this.minutes++
+      }
+    }, 1000)
+  }
+
+  reset(): void {
+    // this.chips = []
+    // this.chipsDeck = []
+    // this.turnOfPlayer1 = true
+    // this.initializeBoard()
+    // this.addPlayerChips()
+    // this.repaint()
+    this.startChronometer()
   }
 }
