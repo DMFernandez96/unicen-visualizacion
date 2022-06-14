5d7c580e87edc0d6a227f5ea6f2868fb54dbd754
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-06-14 20:36:06 -0300

    Added game check winner functionality

--- TP3/src/app/components/pages/playing/Game.ts
+++ TP3/src/app/components/pages/playing/Game.ts
@@ -1,4 +1,3 @@
-import { ThisReceiver } from '@angular/compiler'
 import { Board } from './Board'
 import { Chip } from './Chip'
 import { MeasuresService } from './measures.service'
@@ -67,6 +66,7 @@ export class Game {
         if (this.checkWinner(inserted.column, inserted.row)) {
           this.timer.stop()
           this.turnOfPlayer1 = !this.turnOfPlayer1
+          this.winner = this.turnOfPlayer1 ? 1 : 2
         } else {
           this.timer.changeTimers()
         }
@@ -117,15 +117,126 @@ export class Game {
     return { column: column, row: position }
   }
 
+  // checkWinner(column: number, row: number): boolean {
+  // if (
+  //   column == this.measures.boardWidth - 1 &&
+  //   row == this.measures.boardHeigth - 1
+  // ) {
+  //   this.winner = !this.turnOfPlayer1 ? 1 : 2
+  //   return true
+  // }
+  // return false
+  // }
+
+  // checkWinner(char [][] this.board.chips, int row, int column, char fichaJugada):boolean{
   checkWinner(column: number, row: number): boolean {
-    if (
-      column == this.measures.boardWidth - 1 &&
-      row == this.measures.boardHeigth - 1
-    ) {
-      this.winner = !this.turnOfPlayer1 ? 1 : 2
+    const numberToWin = this.measures.boardWidth - 3
+    const state = this.board.chips[column][row].getState()
+    let chipsSum = 0 //la posición de inicio (donde cayó la ficha) se cuenta en cada función
+
+    //count to left
+    chipsSum += this.countLeft(column, row, state)
+    if (chipsSum >= numberToWin) {
       return true
     }
+    //count to right
+    chipsSum += this.countRight(column, row, state)
+    chipsSum-- //Resto 1 porque la column la sumo yendo a izquieda y a derecha
+    if (chipsSum >= numberToWin) {
+      return true
+    }
+
+    //there is no winner horizontally
+    chipsSum = 0
+
+    //count to down
+    chipsSum += this.countDown(column, row, state)
+    if (chipsSum >= numberToWin) {
+      return true
+    }
+
+    //there is no winner vertically
+    chipsSum = 0
+
+    //count down right diagonal
+    chipsSum += this.countDownRight(column, row, state)
+    // chipsSum-- //Resto 1 porque la celda jugada se cuenta en ambas direcciones de diagonal
+    if (chipsSum >= numberToWin) {
+      return true
+    }
+
+    //there is no winner down right diagonal
+    chipsSum = 0
+
+    //count down left diagonal
+    chipsSum += this.countDownLeft(column, row, state)
+    // chipsSum-- //Resto 1 porque la celda jugada se cuenta en ambas direcciones de diagonal
+    if (chipsSum >= numberToWin) {
+      return true
+    }
+
     return false
+  }
+
+  countRight(column: number, row: number, state: number): number {
+    let equalsQuantity = 0
+    while (
+      column < this.measures.boardWidth &&
+      this.board.chips[column][row].getState() == state
+    ) {
+      equalsQuantity++
+      column++
+    }
+    return equalsQuantity
+  }
+
+  countLeft(column: number, row: number, state: number): number {
+    let equalsQuantity = 0
+    while (column >= 0 && this.board.chips[column][row].getState() == state) {
+      equalsQuantity++
+      column--
+    }
+    return equalsQuantity
+  }
+
+  countDown(column: number, row: number, state: number): number {
+    let equalsQuantity = 0
+    while (
+      row < this.measures.boardHeigth &&
+      this.board.chips[column][row].getState() == state
+    ) {
+      equalsQuantity++
+      row++
+    }
+    return equalsQuantity
+  }
+
+  countDownLeft(column: number, row: number, state: number): number {
+    let equalsQuantity = 0
+    while (
+      row < this.measures.boardHeigth &&
+      column >= 0 &&
+      this.board.chips[column][row].getState() == state
+    ) {
+      equalsQuantity++
+      row++
+      column--
+    }
+    return equalsQuantity
+  }
+
+  countDownRight(column: number, row: number, state: number): number {
+    let equalsQuantity = 0
+    while (
+      row < this.measures.boardHeigth &&
+      column < this.measures.boardWidth &&
+      this.board.chips[column][row].getState() == state
+    ) {
+      equalsQuantity++
+      row++
+      column++
+    }
+    return equalsQuantity
   }
 
   static setWinner(): void {}
