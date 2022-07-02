aca407bd810d267acdc16b2bf8dfe29733159726
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-07-02 11:53:23 -0300

    Added gem generate random and move functionality

---
+++ TP4/src/app/components/pages/playing/game-parallax/Gem.ts
@@ -0,0 +1,9 @@
+export class Gem {
+  color: string
+  bottom: number
+
+  constructor(color: string, bottom: number) {
+    this.color = color
+    this.bottom = bottom
+  }
+}
--- TP4/src/app/components/pages/playing/game-parallax/game-parallax.component.ts
+++ TP4/src/app/components/pages/playing/game-parallax/game-parallax.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
 import { Character } from './Character'
+import { Gem } from './Gem'
 import { Zombie } from './Zombie'
 
 @Component({
@@ -25,9 +26,14 @@ export class GameParallaxComponent implements OnInit {
   zombiesInGame: Zombie[]
   maxZombiesInGame = 4
 
+  gemsInGame: Gem[]
+  gemsColors: string[]
+
   constructor() {
     this.zombiesToChoose = []
     this.zombiesInGame = []
+    this.gemsInGame = []
+    this.gemsColors = ['blue', 'green', 'grey', 'orange', 'pink', 'yellow']
   }
 
   ngOnInit(): void {
@@ -48,6 +54,7 @@ export class GameParallaxComponent implements OnInit {
 
   gameLoop(): void {
     this.createZombies()
+    this.checkGems()
     requestAnimationFrame(this.gameLoop.bind(this))
   }
 
@@ -65,5 +72,26 @@ export class GameParallaxComponent implements OnInit {
     setTimeout(() => {
       this.zombiesInGame.push(new Zombie(this.zombie4Element.nativeElement))
     }, 21000)
+  }
+
+  checkGems(): void {
+    if (this.gemsInGame.length < 10) {
+      //ToDo: check that the position 'right' from the last gem in array almost
+      // 200-300px from right to create a new gem to gain separation between gems
+      // How to get the right position?? Hint: see collisions
+      const gem = new Gem(
+        this.gemsColors[this.randomIntFromInterval(1, this.gemsColors.length)],
+        this.randomIntFromInterval(100, 300)
+      )
+      this.gemsInGame.push(gem)
+      setTimeout(() => {
+        this.gemsInGame.splice(this.gemsInGame.indexOf(gem), 1)
+      }, 6000)
+    }
+  }
+
+  randomIntFromInterval(min: number, max: number): number {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
   }
 }
